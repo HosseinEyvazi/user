936bfc3afff7ce7044d7c0d7d071331bb1f63940
Hoseyn Eyvazi <hossein.eyvazi98@gmail.com>
2023-08-06 15:25:40 +0330

    new features

--- src/Components/adminHome.jsx
+++ src/Components/adminHome.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import User from "./user";
 
 const AdminHome = () => {
   const [usersState, setUsers] = useState([]);
@@ -76,31 +77,7 @@ const AdminHome = () => {
         skeletonLoading()
       ) : (
         filteredUsers.map((oneUsers, index) => (
-          <div
-            className="p-3 flex flex-row items-center bg-gray-100 rounded-3xl m-2"
-            key={index}
-          >
-            <img
-              src={oneUsers.avatar}
-              alt="عکس ندارد"
-              width={130}
-              height={130}
-              className="outline-blue-100  mr-8 rounded-3xl border-blue-200 border-2 shadow-black shadow-lg"
-            />
-
-            <div
-              className="p-6 bg-gray-100  shadow-black shadow-lg rounded-3xl m-4"
-            >
-              <h2 className="text-black mb-2 font-extrabold">
-                {"نام و نام خانوادگی: " + oneUsers.family + "  " + oneUsers.name}
-              </h2>
-              <h1>{"وضعیت خدمت: " + oneUsers.military}</h1>
-              <h1>{"کد ملی: " + oneUsers.national_id}</h1>
-              <h1>{"تاریخ تولد: " + oneUsers.date}</h1>
-              <h1>{"شهر: " + oneUsers.city}</h1>
-              <h1>{"جنسیت: " + (oneUsers.sex === "Female" ? "خانم" : "اقا")}</h1>
-            </div>
-          </div>
+          <User key={index} sex={oneUsers.sex} city={oneUsers.city} date={oneUsers.date} national_id={oneUsers.national_id} military={oneUsers.military} family={oneUsers.family} name={oneUsers.name} avatar={oneUsers.avatar}/>
         ))
       )}
     </>
---
+++ src/Components/user.jsx
@@ -0,0 +1,33 @@
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+
+const User = ({avatar ,sex , city , date , national_id , military , family , name }) => {
+  return (
+    <div>
+      <div
+        className="p-3 flex flex-row items-center bg-gray-100 rounded-3xl m-2"
+        
+      >
+        <img
+          src={avatar}
+          alt="عکس ندارد"
+          width={130}
+          height={130}
+          className="outline-blue-100  mr-8 rounded-3xl border-blue-200 border-2 shadow-black shadow-lg"
+        />
+
+        <div className="p-6 bg-gray-100  shadow-black shadow-lg rounded-3xl m-4">
+          <h2 className="text-black mb-2 font-extrabold">
+            {"نام و نام خانوادگی: " + family + "  " +name}
+          </h2>
+          <h1>{"وضعیت خدمت: " + military}</h1>
+          <h1>{"کد ملی: " + national_id}</h1>
+          <h1>{"تاریخ تولد: " +date}</h1>
+          <h1>{"شهر: " + city}</h1>
+          <h1>{"جنسیت: " + (sex === "Female" ? "خانم" : "اقا")}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
